72301de1a575b2ff621cd7a1b77263b183ebce99
Michael Whidby <michael.whidby@gmail.com>
2011-04-13 21:16:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splitit::Application.config.secret_token = 'e3e85a4c9e3b068cecabd50c16522e5f3998a502f2a4841d67c04b970bf771754b0ff418c01aab1657119229f2c548222bb67ee044cb5701240f07c8c652289a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Splitit::Application.config.session_store :cookie_store, :key => '_splitit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Splitit::Application.config.session_store :active_record_store
